444a180d040ee620bde8e4321ba80b4d9d614718
CookieMonsta89 <jlstan06@gmail.com>
2018-09-17 14:56:58 -0400

    did assignment along with students

--- js/index.js
+++ js/index.js
@@ -40,3 +40,55 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+let newLink = document.createElement('a');
+let navMain = document.querySelector('nav');
+navMain.append(newLink);
+
+let links = document.querySelectorAll('a');
+links[0].innerText = 'Services';
+links[1].innerText = 'Product';
+links[2].innerText = 'Vision';
+links[3].innerText = 'Features';
+links[4].innerText = 'About';
+links[5].innerText = 'Contact';
+links[6].innerText = 'Added Link'
+
+links[6].style.cursor = 'pointer';
+
+
+let bigText = document.querySelector('.cta-text h1');
+bigText.innerText = 'DOM Is Awesome';
+console.log(bigText)
+
+let button = document.querySelector('.cta-text button');
+button.innerText = 'Get Started';
+
+let bigImg = document.getElementById('cta-img');
+bigImg.src = './img/header-img.png'
+
+let headTxt = document.querySelectorAll('h4');
+let pTxt = document.querySelectorAll('p');
+
+headTxt[0].innerText = 'Features';
+pTxt[0].innerText = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+headTxt[1].innerText = 'About';
+pTxt[1].innerText = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+let midImg = document.querySelector('.middle-img');
+midImg.src = './img/mid-page-accent.jpg';
+
+headTxt[2].innerText = 'Services';
+pTxt[2].innerText = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+headTxt[3].innerText = 'Product';
+pTxt[3].innerText = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+headTxt[4].innerText = 'Vision';
+pTxt[4].innerText = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+headTxt[5].innerText = 'Contact';
+pTxt[5].innerText = '123 Way 456 Street Somewhere, USA';
+pTxt[6].innerText = '1 (888) 888-8888';
+pTxt[7].innerText = 'sales@greatidea.io';
+pTxt[8].innerText = 'Copyright Great Idea! 2018';
